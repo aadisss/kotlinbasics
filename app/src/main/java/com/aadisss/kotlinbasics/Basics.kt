cb86a2ef124ba93c094b815d13882c358ec10d57
Adeel Nasir <adeel.nasir@jitips.com>
2025-08-15 01:03:33 +0100

    replaced the if  commit

--- app/src/main/java/com/aadisss/kotlinbasics/Basics.kt
+++ app/src/main/java/com/aadisss/kotlinbasics/Basics.kt
@@ -8,14 +8,16 @@ fun main(){
     println("Rock, Paper or Scissors. Enter your choice!")
     userChoice = readln()
     val randomNumber = (1..3).random()
-    if(randomNumber == 1){
-    computerChoice = "Rock"
-    }
-    else if(randomNumber == 2){
-        computerChoice = "Paper"
-    }
-    else if(randomNumber == 3) {
-        computerChoice = "Scissors"
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
     }
     println(computerChoice)
 }
