13804e97ce8a26c6849d577a0af7dbf3e92826bf
Adeel Nasir <adeel.nasir@jitips.com>
2025-08-16 15:48:28 +0100

    implementation of when statement

--- app/src/main/java/com/aadisss/kotlinbasics/Basics.kt
+++ app/src/main/java/com/aadisss/kotlinbasics/Basics.kt
@@ -9,15 +9,22 @@ fun main(){
     userChoice = readln()
     val randomNumber = (1..3).random()
     when (randomNumber) {
-        1 -> {
-            computerChoice = "Rock"
-        }
-        2 -> {
-            computerChoice = "Paper"
-        }
-        3 -> {
-            computerChoice = "Scissors"
-        }
+        1 -> computerChoice = "Rock"
+        2 -> computerChoice = "Paper"
+        3 -> computerChoice = "Scissors"
     }
     println(computerChoice)
+    val winner = when{
+        userChoice == computerChoice -> "Tie"
+        userChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        userChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        userChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+    if(winner=="Tie"){
+        println("No one won")
+    }
+    else {
+        print("$winner Won")
+    }
 }
